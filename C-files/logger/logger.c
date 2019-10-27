dc3759b918df87e4a717032aea8e790313e99025
yishai.peleg <yishai.peleg@gmail.com>
2019-10-27 18:32:51 +0200

    logger - append

--- C-files/logger/logger.c
+++ C-files/logger/logger.c
@@ -7,6 +7,7 @@ typedef enum status
 	SUCCESS,
 	FAIL,
 	EXIT
+
 } status_t;
 
 typedef struct Operations
@@ -16,4 +17,37 @@ typedef struct Operations
 	status_t (*action_func_t)(const char str, const FILE *file_name);
 } operation_t;
 
+/******** Append lines to a file *******/
+static status_t AppendToFile(const char str, const FILE *file_name)
+{
+	file_name = fopen("new_file", "a+");
 
+	fgets(str, 100, stdin);
+	while (*str != 27)
+	{
+		fputs(str, file_ptr);
+		fgets(str, 100, stdin); 
+	}
+
+	fclose(file_ptr);
+
+
+}
+int main ()
+{
+	FILE *file_ptr = NULL;
+	char str[100];
+
+	file_ptr = fopen("new_file", "a+");
+
+	fgets(str, 100, stdin);
+	while (*str != 27)
+	{
+		fputs(str, file_ptr);
+		fgets(str, 100, stdin); 
+	}
+
+	fclose(file_ptr);
+
+	return 0;
+}
