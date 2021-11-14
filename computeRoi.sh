cf731c6aff74037fcfedf2899ab291dd5acde3f0
Gal N <Gal.Nagar@ge.com>
2021-11-14 14:36:55 +0200

    Changed the ROI computation

--- computeRoi.sh
+++ computeRoi.sh
@@ -13,7 +13,7 @@ manufacture_date=$(date +%s -d $1)
 day_diff=$(( $(( current_date - manufacture_date )) / 86400 )) 
 
 # The computed ROI 
-computed_roi=$(echo "( e(390.7 / $day_diff) * 1000 * $activity ) / $volume" | bc -l)
+computed_roi=$(echo "( e(-$day_diff / 390.7 ) * 1000 * $activity ) / $volume" | bc -l)
 
 echo "Number of days between today and the source's manufacturing day = $day_diff days"
 echo "The activity used = $activity MBq"
